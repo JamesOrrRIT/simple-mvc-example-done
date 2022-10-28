d2c562f7aaae0b14208ef3f9d733ef643636f161
James Orr (RIT Student) <jpo3042@rit.edu>
2022-10-28 13:48:14 -0400

    Pushng the finalized assignment in

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,9 @@
 // pull in our models. This will automatically load the index.js from that folder
 const models = require('../models');
 
-// get the Cat model
+// get the Cat and Dog models
 const { Cat } = models;
+const { Dog } = models;
 
 // default fake data so that we have something to work with until we make a real Cat
 const defaultData = {
@@ -12,6 +13,7 @@ const defaultData = {
 
 // object for us to keep track of the last Cat we made and dynamically update it sometimes
 let lastAdded = new Cat(defaultData);
+let latestAdded = new Dog(defaultData);
 
 // Function to handle rendering the index page.
 const hostIndex = (req, res) => {
@@ -83,142 +85,101 @@ const hostPage3 = (req, res) => {
   res.render('page3');
 };
 
+const hostPage4 = async (req, res) => {
+  try {
+    const docs = await Dog.find({}).lean().exec();
+    return res.render('page4', { dogs: docs });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'faled to find dogs' });
+  }
+};
+
 // Get name will return the name of the last added cat.
 const getName = (req, res) => res.json({ name: lastAdded.name });
 
+const getDogName = (req, res) => res.json({ name: lastAdded.name });
+
 // Function to create a new cat in the database
 const setName = async (req, res) => {
-  /* If we look at views/page2.handlebars, the form has inputs for a firstname, lastname
-     and a number of beds. When this POST request is sent to us, the bodyParser plugin
-     we configured in app.js will store that information in req.body for us.
-  */
   if (!req.body.firstname || !req.body.lastname || !req.body.beds) {
     // If they are missing data, send back an error.
     return res.status(400).json({ error: 'firstname, lastname and beds are all required' });
   }
 
-  /* If they did send all the data, we want to create a cat and add it to our database.
-     We begin by creating a cat that matches the format of our Cat schema. In this case,
-     we define a name and bedsOwned. We don't need to define the createdDate, because the
-     default Date.now function will populate that value for us later.
-  */
   const catData = {
     name: `${req.body.firstname} ${req.body.lastname}`,
     bedsOwned: req.body.beds,
   };
 
-  /* Once we have our cat object set up. We want to turn it into something the database
-     can understand. To do this, we create a new instance of a Cat using the Cat model
-     exported from the Models folder.
-
-     Note that this does NOT store the cat in the database. That is the next step.
-  */
   const newCat = new Cat(catData);
 
-  /* We have now setup a cat in the right format. We now want to store it in the database.
-     Again, because the database and node server are separate things entirely we have no
-     way of being sure the database will work or respond. Because of that, we wrap our code
-     in a try/catch.
-  */
   try {
-    /* newCat is a version of our catData that is database-friendly. If you print it, you will
-       see it has extra information attached to it other than name and bedsOwned. One thing it
-       now has is a .save() function. This function will intelligently add or update the cat in
-       the database. Since we have never saved this cat before, .save() will create a new cat in
-       the database. All calls to the database are async, including .save() so we will await the
-       databases response. If something goes wrong, we will end up in our catch() statement.
-    */
     await newCat.save();
+    lastAdded = newCat;
+    return res.json({
+      name: lastAdded.name,
+      beds: lastAdded.bedsOwned,
+    });
   } catch (err) {
-    /* If something goes wrong while communicating with the database, log the error and send
-       an error message back to the client. Note that our return will return us from the setName
-       function, not just the catch statement. That means we can treat the code below the catch
-       as being our "if the try worked"
-    */
+
     console.log(err);
     return res.status(500).json({ error: 'failed to create cat' });
   }
-
-  /* After our await has resolved, and if no errors have occured during the await, we will end
-     up here. We will update our lastAdded cat to the one we just added. We will then send that
-     cat's data to the client.
-  */
-  lastAdded = newCat;
-  return res.json({
-    name: lastAdded.name,
-    beds: lastAdded.bedsOwned,
-  });
 };
 
 // Function to handle searching a cat by name.
 const searchName = async (req, res) => {
-  /* When the user makes a POST request, bodyParser populates req.body with the parameters
-     as we saw in setName() above. In the case of searchName, the user is making a GET request.
-     GET requests do not have a body, but they can have query parameters. bodyParser will also
-     handle these, and store them in req.query instead.
-
-     If the user does not give us a name to search by, throw an error.
-  */
   if (!req.query.name) {
     return res.status(400).json({ error: 'Name is required to perform a search' });
   }
 
-  /* If they do give us a name to search, we will as the database for a cat with that name.
-     Remember that since we are interacting with the database, we want to wrap our code in a
-     try/catch in case the database throws an error or doesn't respond.
-  */
-  let doc;
   try {
-    /* Just like Cat.find() in hostPage1() above, Mongoose models also have a .findOne()
-       that will find a single document in the database that matches the search parameters.
-       This function is faster, as it will stop searching after it finds one document that
-       matches the parameters. The downside is you cannot get multiple responses with it.
+    const doc = await Cat.findOne({ name: req.query.name }).exec();
 
-       One of three things will occur when trying to findOne in the database.
-        1) An error will be thrown, which will stop execution of the try block and move to
-            the catch block.
-        2) Everything works, but the name was not found in the database returning an empty
-            doc object.
-        3) Everything works, and an object matching the search is found.
-    */
-    doc = await Cat.findOne({ name: req.query.name }).exec();
+    // If we do not find something that matches our search, doc will be empty.
+    if (!doc) {
+      return res.json({ error: 'No cats found' });
+    }
+
+    // Otherwise, we got a result and will send it back to the user.
+    return res.json({ name: doc.name, beds: doc.bedsOwned });
   } catch (err) {
     // If there is an error, log it and send the user an error message.
     console.log(err);
     return res.status(500).json({ error: 'Something went wrong' });
   }
-
-  // If we do not find something that matches our search, doc will be empty.
-  if (!doc) {
-    return res.json({ error: 'No cats found' });
-  }
-
-  // Otherwise, we got a result and will send it back to the user.
-  return res.json({ name: doc.name, beds: doc.bedsOwned });
 };
 
-/* A function for updating the last cat added to the database.
-   Usually database updates would be a more involved process, involving finding
-   the right element in the database based on query, modifying it, and updating
-   it. For this example we will just update the last one we added for simplicity.
-*/
+const setDogName = async (req, res) => {
+  if (!req.body.name || !req.body.breed || !req.body.age) return res.status(400).json({ error: 'You need a name, breed, and age' });
+
+  const dogData = {
+    name: req.body.name,
+    breed: req.body.breed,
+    age: req.body.age,
+  };
+
+  const newDog = new Dog(dogData);
+
+  try {
+    await newDog.save();
+    latestEdition = newDog;
+    return res.json({
+      name: latestEdition.name,
+      breed: latestEdition.breed,
+      age: latestEdition.age,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'Cannot create dog' });
+  }
+};
+
 const updateLast = (req, res) => {
   // First we will update the number of bedsOwned.
   lastAdded.bedsOwned++;
 
-  /* Remember that lastAdded is a Mongoose document (made on line 14 if no new
-     ones were made after the server started, or line 116 if there was). Mongo
-     documents have an _id, which is a globally unique identifier that distinguishes
-     them from other documents. Our mongoose document also has this _id. When we
-     call .save() on a document, Mongoose and Mongo will use the _id to determine if
-     we are creating a new database entry (if the _id doesn't already exist), or
-     if we are updating an existing entry (if the _id is already in the database).
-
-     Since lastAdded is likely already in the database, .save() will update it rather
-     than make a new cat.
-
-     We can use async/await for this, or just use standard promise .then().catch() syntax.
-  */
   const savePromise = lastAdded.save();
 
   // If we successfully save/update them in the database, send back the cat's info.
@@ -234,6 +195,37 @@ const updateLast = (req, res) => {
   });
 };
 
+const updateDogAge = async (req, res) => {
+  if (!req.query.name) {
+    return res.status(400).json({ error: 'Name is required to perform a search' });
+  }
+
+  try {
+    const doc = await Dog.findOne({ name: req.query.name }).exec();
+    console.log(doc);
+    if (!doc) {
+      return res.json({ error: 'No dogs found' });
+    }
+
+    doc.age++;
+
+    const savePromise = doc.save();
+    savePromise.then(() => res.json({
+      name: doc.name,
+      breed: doc.breed,
+      age: doc.age,
+    }));
+
+    return savePromise;
+    // Otherwise, we got a result and will send it back to the user.
+    // return res.json({ name: doc.name, beds: doc.bedsOwned });
+  } catch (err) {
+    // If there is an error, log it and send the user an error message.
+    console.log(err);
+    return res.status(500).json({ error: 'Something went wrong' });
+  }
+};
+
 // A function to send back the 404 page.
 const notFound = (req, res) => {
   res.status(404).render('notFound', {
@@ -247,9 +239,13 @@ module.exports = {
   page1: hostPage1,
   page2: hostPage2,
   page3: hostPage3,
+  page4: hostPage4,
   getName,
+  getDogName,
   setName,
+  setDogName,
   updateLast,
   searchName,
+  updateDogAge,
   notFound,
 };
--- server/router.js
+++ server/router.js
@@ -1,5 +1,3 @@
-// import the controllers
-// This only specifies the folder name, which means it will automatically pull the index.js file
 const controllers = require('./controllers');
 
 // function to attach routes
@@ -16,8 +14,11 @@ const router = (app) => {
   app.get('/page1', controllers.page1);
   app.get('/page2', controllers.page2);
   app.get('/page3', controllers.page3);
+  app.get('/page4', controllers.page4);
   app.get('/getName', controllers.getName);
   app.get('/findByName', controllers.searchName);
+  app.get('/getDogName', controllers.getDogName);
+  app.get('/updateDogLast', controllers.updateDogAge);
 
   // whenever someone goes to the site without a path (AKA the home page), call controllers.index
   // For example www.webpage.com
@@ -29,6 +30,8 @@ const router = (app) => {
   // When someone POSTS to /setName, call controllers.setName
   // For example, a form submission to www.webpage.com/setName
   app.post('/setName', controllers.setName);
+
+  app.post('/setDogName', controllers.setDogName);
 
   // When someone POSTS to /updateLast, call controllers.updateLast
   app.post('/updateLast', controllers.updateLast);
